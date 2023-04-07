66873ea972069922778321c59f1e86abb327eacd
NopeGuy <luisgmsferreira@gmail.com>
2023-04-07 14:53:45 +0100

    Alguns metodos e stuff
    stuff é mais métodos
    -clone
    -toString
    -equals
    -getters e setters

--- src/Encomenda.java
+++ src/Encomenda.java
@@ -1,19 +1,108 @@
 import Items.Artigo;
-
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 public class Encomenda {
     private Collection<Artigo> colecao;
     private String dimensao;
-    private double preço_final;
+    private double preco_final;
     private String estado;
-    private LocalDate data_criação;
+    private LocalDate data_criacao;
 
-    //criar encomenda
-    //remover artigos
-    //adicionar artigos
-    //calcular preço final = set preço final
-    //devolver
+    // Construtor
+    public Encomenda(Collection<Artigo> colecao, String dimensao, double preco_final, String estado, LocalDate data_criacao) {
+        this.colecao = new ArrayList<>(colecao);
+        this.dimensao = dimensao;
+        this.preco_final = preco_final;
+        this.estado = estado;
+        this.data_criacao = data_criacao;
+    }
+    // Construtor de cópia
+    public Encomenda(Encomenda encomenda) {
+        this(encomenda.colecao, encomenda.dimensao, encomenda.preco_final, encomenda.estado, encomenda.data_criacao);
+    }
 
+    // Construtor vazio
+    public Encomenda() {
+        this(new ArrayList<>(), "", 0.0, "", LocalDate.now());
+    }
+
+
+    // Métodos de acesso
+    public Collection<Artigo> getColecao() {
+        return new ArrayList<>(colecao);
+    }
+
+    public String getDimensao() {
+        return dimensao;
+    }
+
+    public double getPreco_final() {
+        return preco_final;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public LocalDate getData_criacao() {
+        return data_criacao;
+    }
+
+    // Métodos de modificação
+    public void adicionarArtigo(Artigo artigo) {
+        colecao.add(artigo);
+    }
+
+    public void removerArtigo(Artigo artigo) {
+        colecao.remove(artigo);
+    }
+
+    public void calcularPrecoFinal() {
+        double total = 0.0;
+        for (Artigo artigo : colecao) {
+            total += artigo.getPreco();
+        }
+        this.preco_final = total;
+    }
+
+    // Métodos sobrescritos
+    @Override
+    public Encomenda clone() {
+        try {
+            Encomenda clone = (Encomenda) super.clone();
+            clone.colecao = new ArrayList<>(colecao);
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Encomenda)) return false;
+        Encomenda e = (Encomenda) o;
+        return Objects.equals(colecao, e.colecao) &&
+                Objects.equals(dimensao, e.dimensao) &&
+                Double.compare(preco_final, e.preco_final) == 0 &&
+                Objects.equals(estado, e.estado) &&
+                Objects.equals(data_criacao, e.data_criacao);
+    }
+    @Override
+    public String toString() {
+        return "Encomenda{" +
+                "colecao=" + colecao +
+                ", dimensao='" + dimensao + '\'' +
+                ", preco_final=" + preco_final +
+                ", estado='" + estado + '\'' +
+                ", data_criacao=" + data_criacao +
+                '}';
+    }
 }
+
+//criar encomenda
+//adicionar artigos
+//calcular preço final = set preço final
+//devolver
--- src/User.java
+++ src/User.java
@@ -1,6 +1,8 @@
 import Items.Artigo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class User {
     private String user_id; //gerado automaticamente
@@ -10,9 +12,21 @@ public class User {
     private List<Artigo> vendeu;
     private List<Artigo> comprou;
 
+    public User(String email, String morada, int num_fiscal) {
+        this.user_id = generateUserId();
+        this.email = email;
+        this.morada = morada;
+        this.num_fiscal = num_fiscal;
+        this.vendeu = new ArrayList<>();
+        this.comprou = new ArrayList<>();
+    }
 
     //Lembrete tipos primitos não precisam de clone mesmo numa estratégia de composição
 
+    private String generateUserId() {
+        // lógica para gerar o user_id
+        return "user_" + Math.round(Math.random() * 1000);
+    }
 
     //Gets e sets
     //vendeu e comprou ainda por fazer, têm de ser pensados depois
@@ -41,5 +55,42 @@ public class User {
     }
     public void setMorada(String morada) {
         this.morada = morada;
+    }
+
+    @Override
+    public User clone() {
+        try {
+            User clone = (User) super.clone();
+            clone.vendeu = new ArrayList<>(vendeu);
+            clone.comprou = new ArrayList<>(comprou);
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        User u = (User) o;
+        return num_fiscal == u.num_fiscal &&
+                Objects.equals(user_id, u.user_id) &&
+                Objects.equals(email, u.email) &&
+                Objects.equals(morada, u.morada) &&
+                Objects.equals(vendeu, u.vendeu) &&
+                Objects.equals(comprou, u.comprou);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id='" + user_id + '\'' +
+                ", email='" + email + '\'' +
+                ", morada='" + morada + '\'' +
+                ", num_fiscal=" + num_fiscal +
+                ", vendeu=" + vendeu +
+                ", comprou=" + comprou +
+                '}';
     }
 }
